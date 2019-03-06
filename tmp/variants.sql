dd9aa2de6c71f357c9c56e29a0126bd5ecb8a1e6
auvalade <auvalade@hotmail.com>
2019-03-06 13:46:01 +0800

    Updates query JOIN issue and fixes typo in tmp/variants.sql

--- tmp/variants.sql
+++ tmp/variants.sql
@@ -1,10 +1,10 @@
 SELECT
-    f.product_id AS productId
+    v.product_id AS productId,
     v.id AS skuId,
     p.purchase_prices AS purchasePrices,
     i.urls AS urls,
     inv.inventory_count AS inventoryCount
 FROM `raw.variants` v
-LEFT JOIN `tmp.item_purchase_prices` p
-LEFT JOIN `tmp.variant_images` i
-LEFT JOIN `tmp.inventory_items` inv
+    LEFT JOIN `tmp.inventory_items` inv ON inv.variant_id = v.id
+    LEFT JOIN `tmp.item_purchase_prices` p ON p.order_item_id = inv.order_item_id
+    LEFT JOIN `tmp.variant_images` i ON i.variant_id = v.id
